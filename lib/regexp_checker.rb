c8977248fe749e2d68a06e1831de538205906dd9
shoji <s.shoji39@gmail.com>
2021-01-05 20:26:24 +0900

    例外処理を組み込む

--- lib/regexp_checker.rb
+++ lib/regexp_checker.rb
@@ -3,6 +3,14 @@ text = gets.chomp
 print 'Pattern?: '
 pattern = gets.chomp
 
+begin
+  print 'Pattern?: '
+  pattern = gets.chomp
+  regexp = Regexp.new(pattern)
+rescue => e
+  puts "Invalid pattern: #{e.message} "
+  retry
+end
 regexp = Regexp.new(pattern)
 matches = text.scan(regexp)
 if matches.size > 0
